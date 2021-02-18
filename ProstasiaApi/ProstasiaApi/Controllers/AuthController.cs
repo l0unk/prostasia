89ace7e293087773ad10f1077f8dbd093e4a7f79
gstpsk <gstpsk@outlook.com>
2021-02-18 23:24:41 +0100

    Does gitignore work

---
+++ ProstasiaApi/ProstasiaApi/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProstasiaApi.Controllers
+{
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> Login()
+        {
+            
+
+            return Ok();
+        }
+    }
+}
---
+++ ProstasiaApi/ProstasiaApi/Database.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.IO;
+
+namespace ProstasiaApi
+{
+    public class Database
+    {
+        public string LoadConnStr()
+        {
+            string[] connStrArr = File.ReadAllLines("conn.txt");
+            return connStrArr[0];
+        }
+        
+        public async Task<string> GetUserPassword(string username)
+        {
+            MongoClient dbClient = new MongoClient(LoadConnStr());
+            var db = dbClient.GetDatabase("Prostasia");
+            var collection = db.GetCollection<BsonDocument>("Users");
+            var filter = Builders<BsonDocument>.Filter.Eq("username", username);
+            var result = collection.Find(filter).FirstOrDefault();
+            if (result != null)
+            {
+                return result.GetValue("password").ToString();
+            } else
+            {
+                return null;
+            }
+        }
+    }
+}
