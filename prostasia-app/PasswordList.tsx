c2f346fb9372d3763132a485bcd180d04acf830f
lounk <l0unk@outlook.com>
2021-02-25 00:16:07 +0100

    now using react instead of native

---
+++ prostasia-app/PasswordList.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { View } from 'react-native';
+
+class PasswordList extends React.Component {
+    constructor(props:any) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <View>
+                <Text>Hi</Text>
+            </View>
+        )
+    }
+
+}
+export default PasswordList;
